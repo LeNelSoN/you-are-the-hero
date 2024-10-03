6d1d5e2700cef8600ed529f46fcf02409a5110fd
de l'Hamaide <delhamaide.florent@gmail.com>
2024-10-03 16:14:58 +0200

    feat(story): securized story creation process
    add role EDITOR on creation and update story
    
    feat #27

---
+++ src/main/java/fr/nelson/you_are_the_hero/exception/BadOwnerStoryException.java
@@ -0,0 +1,7 @@
+package fr.nelson.you_are_the_hero.exception;
+
+public class BadOwnerStoryException extends Exception{
+    public BadOwnerStoryException(String message) {
+        super(message);
+    }
+}
