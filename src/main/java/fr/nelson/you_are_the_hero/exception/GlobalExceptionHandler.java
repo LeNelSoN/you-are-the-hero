a1c2faa2ffba4983716b40de6bba504b4ec87176
LeNelSoN <nelis.valentin@gmail.com>
2024-10-26 17:03:42 +0200

    refactor(handleexception): create global method for responseEntity and and handleGlobalException

--- src/main/java/fr/nelson/you_are_the_hero/exception/GlobalExceptionHandler.java
+++ src/main/java/fr/nelson/you_are_the_hero/exception/GlobalExceptionHandler.java
@@ -9,46 +9,62 @@ import org.springframework.web.context.request.WebRequest;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(TokenExpiredException.class)
-    public ResponseEntity<?> handleTokenExpiredException(TokenExpiredException ex, WebRequest request) {
+    private ResponseEntity<ErrorDetails> createErrorResponse(Exception ex, HttpStatus status, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorDetails, status);
+    }
+
+    @ExceptionHandler(AdminAlreadyExistException.class)
+    public ResponseEntity<ErrorDetails> handleAdminAlreadyExistException(AdminAlreadyExistException ex, WebRequest request) {
+        return createErrorResponse(ex,HttpStatus.CONFLICT, request);
+    }
+
+    @ExceptionHandler(TokenExpiredException.class)
+    public ResponseEntity<ErrorDetails> handleTokenExpiredException(TokenExpiredException ex, WebRequest request) {
+        return createErrorResponse(ex,HttpStatus.UNAUTHORIZED, request);
     }
 
     @ExceptionHandler(UserAllreadyExistException.class)
-    public ResponseEntity<?> handleUserAllreadyExistException(UserAllreadyExistException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorDetails> handleUserAllreadyExistException(UserAllreadyExistException ex, WebRequest request) {
+        return createErrorResponse(ex,HttpStatus.CONFLICT, request);
     }
 
     @ExceptionHandler(StoryNotFoundException.class)
-    public ResponseEntity<?> handleStoryNotFoundException(StoryNotFoundException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorDetails> handleStoryNotFoundException(StoryNotFoundException ex, WebRequest request) {
+        return createErrorResponse(ex,HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(SceneAlreadyExistsException.class)
-    public ResponseEntity<?> handleSceneAlreadyExistsException(SceneAlreadyExistsException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorDetails> handleSceneAlreadyExistsException(SceneAlreadyExistsException ex, WebRequest request) {
+        return createErrorResponse(ex,HttpStatus.CONFLICT, request);
     }
 
     @ExceptionHandler(InvalidTokenException.class)
-    public ResponseEntity<?> handleInvalidTokenException(InvalidTokenException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.FORBIDDEN);
+    public ResponseEntity<ErrorDetails> handleInvalidTokenException(InvalidTokenException ex, WebRequest request) {
+        return createErrorResponse(ex,HttpStatus.FORBIDDEN, request);
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<?> handleInvalidCredentialsException(InvalidCredentialsException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<ErrorDetails> handleInvalidCredentialsException(InvalidCredentialsException ex, WebRequest request) {
+        return createErrorResponse(ex,HttpStatus.UNAUTHORIZED, request);
     }
 
     @ExceptionHandler(BadOwnerStoryException.class)
-    public ResponseEntity<?> handleBadOwnerStoryException(BadOwnerStoryException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorDetails> handleBadOwnerStoryException(BadOwnerStoryException ex, WebRequest request) {
+        return createErrorResponse(ex, HttpStatus.NOT_FOUND, request);
     }
 
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorDetails> handleRuntimeException(RuntimeException ex, WebRequest request) {
+        return createErrorResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGeneralException(Exception ex, WebRequest request) {
+        ErrorDetails errorResponse = new ErrorDetails(
+                "An error occurred",
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
